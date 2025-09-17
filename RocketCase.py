a1d3c46653bd7ecc06eb238a1947966ca68ab09f
skrish18 <skrish18@terpmail.umd.edu>
2025-09-17 19:22:03 -0400

    Updated code, added documentation

--- RocketCase.py
+++ RocketCase.py
@@ -2,109 +2,157 @@ from Engine import Engine
 import math
 import numpy as np
 
+
 class RocketCase:
     """
+    The RocketCase class is a wrapper class that performed trend analyses given the stage 1 and stage 2 engines, as well as the corresponding inert mass fraction and payload mass.
     """
-    def __init__(self, dVtot : float, mPL : float, deltas : tuple[float,float], engines : tuple[Engine,Engine]):
-      self.dVtot = dVtot
-      self.mPL = mPL
-      self.deltas = deltas
-      self.engines = engines
+
+    def __init__(
+        self,
+        dVtot: float,
+        mPL: float,
+        deltas: tuple[float, float],
+        engines: tuple[Engine, Engine],
+    ):
+        """
+        :param dVtot: Total Delta-V allocated for the mission
+        :type dVtot: float
+
+        :param mPL:   Payload mass allocated for the mission
+        :type mPL:   float
+
+        :param deltas: Inert mass fraction for the first and second stage
+        :type deltas: Tuple[float,float]
+
+        :param engines: Stage 1 and Stage 2 Engine objects representing the propellant mixtures used.
+        :type engines: Tuple[`Engine`,`Engine`]
+        """
+        self.dVtot = dVtot
+        self.mPL = mPL
+        self.deltas = deltas
+        self.engines = engines
 
     @property
     def gravity(self):
+        """Returns the gravity constant used by operations done inside RocketCase."""
         return 9.81
-    
+
     def findMasses(self, X):
-      #Input: dV Fraction, total dV desired, Inert Mass Ratio
-      #Returns two dictioaries for S1,S2 containing key-value pairs: m0,m_in,m_pr
-      g = self.gravity #maybe use the @property here unsure
-      # ISP1 = self.engines[0].Isp1 #doesnt work
-      # ISP2 = self.engines[1].Isp2 #"
-      ISP1 = self.engines[0].Isp
-      ISP2 = self.engines[1].Isp
+        """Finds the masses for the first and second stage given a Delta-V fraction split.
 
-      dV1 = self.dVtot * X
-      dV2 = self.dVtot * (1-X)
+        :param X: Percent DeltaV to allocate to the first stage. Second stage allocation is 1-X.
+        :type X:  float
 
-      #Stage 2 Calcs
-      r2 = math.exp(-dV2/(g*ISP2))
-      lambda2 = r2 - self.deltas[1]
+        :returns: Mass1 and Mass2, or the stage 1 and stage 2 total, inert, and propellant mass required as dictionaries. The dictionaries can be accesssed with the keys: `m0`,`m_in`,`m_pr`
+        :rtype: Tuple[Dict[str, float], Dict[str, float]]
+        """
+        g = self.gravity
+        # ISP1 = self.engines[0].Isp1 #doesnt work
+        # ISP2 = self.engines[1].Isp2 #"
+        ISP1 = self.engines[0].Isp
+        ISP2 = self.engines[1].Isp
 
-      #Mass Calcs Stage 2
-      m02 = self.mPL / (r2-self.deltas[1])
-      m_in2 = m02*self.deltas[1]
-      m_pr2 = m02 - self.mPL - m_in2
+        dV1 = self.dVtot * X
+        dV2 = self.dVtot * (1 - X)
 
-      Mass2 = {"m0":m02,
-               "m_in":m_in2,
-               "m_pr":m_pr2} #dict with stage 2 mass values
-      #Stage 1 Calcs
-      r1 = math.exp(-dV1/(g*ISP1))
-      
-      #Mass Calcs Stage 1
-      m01 = m02 / (r1-self.deltas[0]) #we use m02 at the payload mass for stage 1
-      m_in1 = m01*self.deltas[0]
-      m_pr1 = m01 - m02 - m_in1
-      Mass1 = {"m0":m01,
-               "m_in":m_in1,
-               "m_pr":m_pr1} #dict with stage 1 mass values
-      return Mass1,Mass2
+        # Stage 2 Calcs
+        r2 = math.exp(-dV2 / (g * ISP2))
+        lambda2 = r2 - self.deltas[1]
 
-    def MassTrends(self, X):
-      #Input: dV Fraction (linear iter), engines class (in particular the ISP's)
-      #Returns XY array of dV and m0
+        # Mass Calcs Stage 2
+        m02 = self.mPL / (r2 - self.deltas[1])
+        m_in2 = m02 * self.deltas[1]
+        m_pr2 = m02 - self.mPL - m_in2
 
-      # #Assigment's
-      # ISP1 = self.engines[0].Isp1 #I'm not sure how to engines class is defined but this is where I'd extract the useful parameters
-      # ISP2 = self.engines[1].Isp2
+        Mass2 = {
+            "m0": m02,
+            "m_in": m_in2,
+            "m_pr": m_pr2,
+        }  # dict with stage 2 mass values
+        # Stage 1 Calcs
+        r1 = math.exp(-dV1 / (g * ISP1))
 
-      # pre-allocate as float array filled with NaN to avoid type warnings
-      m0List = np.full(len(X), np.nan, dtype=float)
-      S1List = []
-      S2List = []
-      for i, xi in enumerate(X):
-        m1,m2 = self.findMasses(xi)
-        S1List.append(m1)
-        S2List.append(m2)
+        # Mass Calcs Stage 1
+        m01 = m02 / (r1 - self.deltas[0])  # we use m02 at the payload mass for stage 1
+        m_in1 = m01 * self.deltas[0]
+        m_pr1 = m01 - m02 - m_in1
+        Mass1 = {
+            "m0": m01,
+            "m_in": m_in1,
+            "m_pr": m_pr1,
+        }  # dict with stage 1 mass values
+        return Mass1, Mass2
 
-        # valid number check (because there is a "maximum" deltaV a stage can provide)
-        try:
-          if m1["m0"] > 0:
-            m0List[i] = m1["m0"]
-          else:
-            m0List[i] = np.nan
-        except Exception:
-          m0List[i] = np.nan
+    def MassTrends(self, X: list[float]):
+        """Returns the mass trends given a list of Delta-V fractions.
 
-      return X,m0List,(S1List,S2List)
+        :param X: List of percents of DeltaV to allocate to the first stage. Second stage allocation is 1-X.
+        :type X: list[float]
 
-    def CostTrends(self,X):
-        #Input: dV Fraction (linear iter)
-        #Returns XY array of dV and cost
+        :returns: X, total masses, and a list of stage 1 and stage 2 masses for a certain Delta-V fraction
+        :rtype: Tuple[list[float], list[float], Tuple[list[Dict[str,float]]], list[Dict[str,float]]]
+        """
 
-        #Pre-allocating the costs as float arrays to allow np.nan
+        # Pre-allocate as float array filled with NaN to avoid type warnings
+        # Initial mass list, and a list of the dictionaries generated by findMasses
+        m0List = np.full(len(X), np.nan, dtype=float)
+        S1List = []
+        S2List = []
+
+        # Enumerate over all X values given
+        for i, xi in enumerate(X):
+            m1, m2 = self.findMasses(xi)
+            S1List.append(m1)
+            S2List.append(m2)
+
+            # Sometimes findMasses generates a negative mass. Guard against illogical values.
+            # try:
+            m0List[i] = m1["m0"] if m1["m0"] > 0 else np.nan
+            # except Exception:
+            # m0List[i] = np.nan
+
+        return X, m0List, (S1List, S2List)
+
+    def CostTrends(self, X):
+        """Returns the cost trends given a list of Delta-V fractions.
+
+        :param X: List of percents of DeltaV to allocate to the first stage. Second stage allocation is 1-X.
+        :type X: list[float]
+
+        :returns: X, total masses, and a list of stage 1 and stage 2 masses for a certain Delta-V fraction
+        :rtype: Tuple[list[float], list[Dict[str,list[float]]]]
+        """
+
+        # Input: dV Fraction (linear iter)
+        # Returns XY array of dV and cost
+
+        # Pre-allocating the costs as float arrays to allow np.nan
         Costs = {
             "S1": np.full(len(X), np.nan, dtype=float),
             "S2": np.full(len(X), np.nan, dtype=float),
-            "Total": np.full(len(X), np.nan, dtype=float)
+            "Total": np.full(len(X), np.nan, dtype=float),
         }
 
         for i, xi in enumerate(X):
-            #Finds Masses then calculats costs -- adding to dictionary
+            # Finds Masses then calculats costs -- adding to dictionary
             m1, m2 = self.findMasses(xi)
-            if m1["m0"] <= 0 or m2["m0"] <= 0:
-                Costs["S1"][i] = np.nan
-                Costs["S2"][i] = np.nan
-                Costs["Total"][i] = np.nan
-            else:
-                Costs["S1"][i] = 13.52*m1["m_in"]**0.55
-                Costs["S2"][i] = 13.52*m2["m_in"]**0.55
-                Costs["Total"][i] = Costs["S1"][i] + Costs["S2"][i]
+
+            s1 = 13.52 * m1["m_in"] ** 0.55
+            s2 = 13.52 * m2["m_in"] ** 0.55
+            Costs["S1"][i], Costs["S2"][i], Costs["Total"][i] = (
+                (np.nan, np.nan, np.nan)
+                if m1["m0"] <= 0 or m2["m0"] <= 0
+                else (s1, s2, s1 + s2)
+            )
 
         return X, Costs
 
-    def findCost(self,m_in):
-        #Input: dV Fraction, Inert Mass
-        #Returns cost of the rocket
-        return 13.52*m_in**0.55
+    @staticmethod
+    def findCost(m_in):
+        """Uses the cost heuristic to find the mass in millions of dollars.
+
+        :param m_in: inert mass in kg
+        :type m_in: float
+        """
+        return 13.52 * m_in**0.55
--- Solver.py
+++ Solver.py
@@ -1,51 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#first stage delta V fraction vs mass
+# first stage delta V fraction vs mass
+
 
 class Solver:
-  def __init__(self, RocketCase):
-    self.Rocket = RocketCase
-    # Restrict search for dV fraction to 35% - 65% to match main.py
-    self.X = np.linspace(0.35,0.65,61)
+    def __init__(self, RocketCaseObject):
+        self.RocketCaseObject = RocketCaseObject
+        self.X, self.m0s, self.StageMasses = self.RocketCaseObject.MassTrends(
+            np.linspace(0.35, 0.65, 61)
+        )
+        _, self.Costs = self.RocketCaseObject.CostTrends(self.X)
 
-  def MinimumMass(self):
-  #Finds the minimum mass solution and returns the X split value and the mass(es), additionally it creates a pretty plot
-    X,m0s,StageMasses = self.Rocket.MassTrends(self.X)
-    minIndex = np.nanargmin(m0s)
-    m0s = [m / 1000 for m in m0s]
-    fig,ax = plt.subplots(figsize=(10,6))
-    ax.plot(X,m0s,label="m0")
-    ax.plot(X[minIndex],m0s[minIndex],'ro',label="Minimum mass: %.2f metric tons" %(m0s[minIndex]),markersize=12)
-    ax.grid(True)
-    ax.set_xlabel("dV Fraction")
-    ax.set_ylabel("m0 (Wet mass) Metric Tonnes")
-    # Limit y-axis to 0 - 30,000 metric tonnes per user request
-    ax.set_ylim(0, 30000)
-    ax.set_title("m0 vs dV Fraction -- S1: %s, S2: %s" %(self.Rocket.engines[0].Name,self.Rocket.engines[1].Name))
-    ax.legend()
+    def PlotMinimumMass(self):
+        # Finds the minimum mass solution and returns the X split value and the mass(es), additionally it creates a pretty plot
+        minIndex = np.nanargmin(self.m0s)
+        m0s = [m / 1000 for m in self.m0s]
 
-    return X[minIndex],m0s[minIndex],(StageMasses[0][minIndex],StageMasses[1][minIndex]),fig
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(self.X, m0s, label="m0")
+        ax.plot(
+            self.X[minIndex],
+            m0s[minIndex],
+            "ro",
+            label="Minimum mass: %.2fkg" % (m0s[minIndex]),
+            markersize=12,
+        )
+        ax.grid(True)
+        ax.set_xlabel("dV Fraction")
+        ax.set_ylabel("m0 (Wet mass) Metric Tonnes")
+        ax.set_title(
+            f"m0 vs dV Fraction -- S1: {self.RocketCaseObject.engines[0].Name}, S2: {self.RocketCaseObject.engines[1].Name}"
+        )
+        ax.legend()
 
-  def MinimumCost(self):
-    #finds the minimum cost solution and returns the X split value and the cost, additionally it creates a pretty plot
-    X,Costs = self.Rocket.CostTrends(self.X)
-    minIndex = np.nanargmin(Costs["Total"])
-    m1,m2 = self.Rocket.findMasses(X[minIndex]) #gets masses at minimum. prolly useful
+        return (
+            self.X[minIndex],
+            m0s[minIndex],
+            (self.StageMasses[0][minIndex], self.StageMasses[1][minIndex]),
+            fig,
+        )
 
-    fig,ax = plt.subplots(figsize=(10,6))
-    # self.__Plot(ax,X,Costs,POI=(X[minIndex],Costs[minIndex]),POI_name="Minimum Cost Point")
-    ax.plot(X,np.array(Costs["Total"]/1000),label="Total Cost")
-    ax.plot(X,np.array(Costs["S1"]/1000),label="Stage 1 Cost")
-    ax.plot(X,np.array(Costs["S2"]/1000),label="Stage 2 Cost")
-    ax.axvline(X[minIndex], color='r', linestyle='--', linewidth=2,label="Minimum cost: $%.2fB" %(Costs["Total"][minIndex]/1000))
-    ax.grid(True)
-    ax.legend()
-    ax.set_xlabel("dV Fraction")
-    ax.set_ylabel("Cost $B")
-    # Limit y-axis to 0 - 50 billion dollars
-    ax.set_ylim(0, 50)
-    ax.set_title("Cost vs dV Fraction -- S1: %s, S2: %s" %(self.Rocket.engines[0].Name,self.Rocket.engines[1].Name))
+    def PlotMinimumCost(self):
+        # finds the minimum cost solution and returns the X split value and the cost, additionally it creates a pretty plot
+        minIndex = np.nanargmin(self.Costs["Total"])
+        m1, m2 = self.RocketCaseObject.findMasses(
+            self.X[minIndex]
+        )  # gets masses at minimum. prolly useful
 
+        fig, ax = plt.subplots(figsize=(10, 6))
+        # self.__Plot(ax,X,Costs,POI=(X[minIndex],Costs[minIndex]),POI_name="Minimum Cost Point")
+        ax.plot(self.X, self.Costs["Total"], label="Total Cost")
+        ax.plot(self.X, self.Costs["S1"], label="Stage 1 Cost")
+        ax.plot(self.X, self.Costs["S2"], label="Stage 2 Cost")
+        ax.axvline(
+            self.X[minIndex],
+            color="r",
+            linestyle="--",
+            linewidth=2,
+            label=f"Minimum cost: ${self.Costs['Total'][minIndex]:.2f}M",
+        )
+        ax.grid(True)
+        ax.legend()
+        ax.set_xlabel("dV Fraction")
+        ax.set_ylabel("Cost $M")
+        ax.set_title(
+            f"Cost vs dV Fraction -- S1: {self.RocketCaseObject.engines[0].Name}, S2: {self.RocketCaseObject.engines[1].Name}"
+        )
 
-    return X[minIndex],(Costs["Total"][minIndex],Costs["S1"][minIndex],Costs["S2"][minIndex]) ,(m1,m2),fig
+        return (
+            self.X[minIndex],
+            (
+                self.Costs["Total"][minIndex],
+                self.Costs["S1"][minIndex],
+                self.Costs["S2"][minIndex],
+            ),
+            (m1, m2),
+            fig,
+        )
